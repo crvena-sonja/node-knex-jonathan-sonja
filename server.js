b2ce79ed3a971b9d4b02b607b4b8c4f04ba3dbfd
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-05-31 12:38:17 -0400

    Do drills 8-13

--- server.js
+++ server.js
@@ -52,5 +52,95 @@ app.get('/restaurants/6', (req, res) => {
     .then(results => res.json(results));
 });
 
+app.get('/restaurants/7', (req, res) => {
+
+  knex('restaurants')
+    .where('cuisine', 'Italian')
+    .whereIn('address_zipcode', ['10012','10013, 10014'])
+    .then(results => res.json(results));
+});
+
+app.get('/restaurants/8', (req, res) => {
+
+  knex('restaurants').insert([{name: 'Byte Cafe',
+    borough: 'Brooklyn',
+    cuisine: 'coffee',
+    address_building_number: '123',
+    address_street: 'Atlantic Avenue',
+    address_zipcode: '11231'}])
+    .then(results => res.json(results));
+});
+
+app.get('/restaurants/9', (req, res) => {
+
+  knex('restaurants').insert([{name: 'Food Place',
+    borough: 'Brooklyn',
+    cuisine: 'pizza',
+    address_building_number: '456',
+    address_street: 'Pacific Avenue',
+    address_zipcode: '11001'}])
+    .returning(['id','name'])
+    .then(results => res.json(results));
+});
+
+app.get('/restaurants/10', (req, res) => {
+
+  knex('restaurants').insert([{name: 'Food Place 2',
+    borough: 'Brooklyn',
+    cuisine: 'Burgers',
+    address_building_number: '987',
+    address_street: 'Atlantic Avenue',
+    address_zipcode: '11231'},
+  {name: 'Food Place 3',
+    borough: 'Brooklyn',
+    cuisine: 'Thai',
+    address_building_number: '345',
+    address_street: 'Atlantic Avenue',
+    address_zipcode: '11231'},
+  {name: 'Food Place 4',
+    borough: 'Brooklyn',
+    cuisine: 'steak',
+    address_building_number: '236',
+    address_street: 'Atlantic Avenue',
+    address_zipcode: '11231'}], ['id','name'])
+    .then(results => res.json(results));
+});
+
+app.get('/restaurants/11', (req, res) => {
+
+  knex('restaurants')
+  .where('nyc_restaurant_id', '30191841')
+  .update({
+    name: 'DJ Reynolds Pub and Restaurant3425'
+  })
+  .then(results => res.json(results));
+});
+
+app.get('/restaurants/12', (req, res) => {
+
+  knex('grades')
+  .where('id', '10')
+  .del()
+  .then(results => res.json(results));
+});
+
+app.get('/restaurants/13', (req, res) => {
+
+  knex('restaurants')
+  .where('id', '22')
+  .del()
+  .then(results => res.json(results))
+  .catch(function(error) {
+    console.error(error);
+  });
+});
+
+app.get('/restaurants/14', (req, res) => {
+
+  knex('grades')
+  .where('id', '10')
+  .del()
+  .then(results => res.json(results));
+});
 
 app.listen(PORT);
